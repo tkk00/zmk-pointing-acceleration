8d7c4c837a605838b92ea8442967b9ecbfb41197
tkk00 <takaki.o.h12@gmail.com>
2026-01-05 00:32:23 +0900

    Update input_processor_accel.c

--- src/input_processor_accel.c
+++ src/input_processor_accel.c
@@ -1,7 +1,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <drivers/input_processor.h>
+#include #include <zmk/drivers/input_processor.h>
 #include <zephyr/dt-bindings/input/input-event-codes.h>
 #include <stdlib.h>  // abs()
 
